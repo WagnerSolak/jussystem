84baeed97c860ecbf8c38adf61bba67d9513cb56
WagnerSolak <wwwagnerww@gmail.com>
2019-02-07 18:05:37 -0200

    Criado pagina de Erro e implementado Log4j

--- src/main/java/com/jussystem/util/jsf/JsfExceptionHandler.java
+++ src/main/java/com/jussystem/util/jsf/JsfExceptionHandler.java
@@ -12,8 +12,13 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class JsfExceptionHandler extends ExceptionHandlerWrapper {
 
+	private static Log log = LogFactory.getLog(JsfExceptionHandler.class);
+	
 	private ExceptionHandler wrapped;
 
 	public JsfExceptionHandler(ExceptionHandler wrapped) {
@@ -41,6 +46,10 @@ public class JsfExceptionHandler extends ExceptionHandlerWrapper {
 				if (exception instanceof ViewExpiredException) {
 					handled = true;
 					redirect("/");
+				}else {
+					handled = true;
+					log.error("Erro de sistema: " + exception.getMessage(), exception);
+					redirect("/Erro.xhtml");
 				}
 
 			} finally {
