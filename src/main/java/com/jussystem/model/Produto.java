3f5d031eb1edf3fdbcee20d256e3427c04717c95
WagnerSolak <wwwagnerww@gmail.com>
2019-02-08 14:09:47 -0200

    Criado model de Categoria e Produto

---
+++ src/main/java/com/jussystem/model/Produto.java
@@ -0,0 +1,77 @@
+package com.jussystem.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Produto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String valorUnitario;
+	private String estoque;
+	
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(String valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	public String getEstoque() {
+		return estoque;
+	}
+	public void setEstoque(String estoque) {
+		this.estoque = estoque;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
